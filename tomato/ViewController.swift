84544d9db54f08ab6bab77f5080821c85835fc7c
sasakazu <sasakura.company@gmail.com>
2018-05-09 17:13:06 +0900

    first commit

---
+++ tomato/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tomato
+//
+//  Created by 笹倉 一也 on 2018/05/09.
+//  Copyright © 2018年 笹倉 一也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
