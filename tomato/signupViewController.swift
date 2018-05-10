e44b303d66d348dd910a45db155cea7af69a2da8
sasakazu <sasakura.company@gmail.com>
2018-05-10 18:58:54 +0900

    log in sign up done...

---
+++ tomato/signupViewController.swift
@@ -0,0 +1,28 @@
+//
+//  signupViewController.swift
+//  tomato
+//
+//  Created by 笹倉 一也 on 2018/05/10.
+//  Copyright © 2018年 笹倉 一也. All rights reserved.
+//
+
+import UIKit
+
+class signupViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
