6df3263a5b7944ef4853dfa324e92b7d947fe42a
sasakazu <sasakura.company@gmail.com>
2018-05-12 19:04:52 +0900

    acount static table ......

---
+++ acountViewController.swift
@@ -0,0 +1,34 @@
+//
+//  acountViewController.swift
+//  
+//
+//  Created by 笹倉 一也 on 2018/05/12.
+//
+
+import UIKit
+
+class acountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
